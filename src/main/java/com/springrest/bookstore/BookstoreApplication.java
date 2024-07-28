31f698ed38af26b6236147b2115b8923af167dd8
Sourabh <kumarsaurav1002@gmail.com>
2024-07-28 11:54:18 +0530

    springrest

---
+++ src/main/java/com/springrest/bookstore/BookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.springrest.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springrest/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springrest.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
